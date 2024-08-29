bb8b8f4538b5bc308207913c6d327633a235b07d
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-08-29 11:25:18 +0500

    update

--- mini project/app.py
+++ mini project/app.py
@@ -6,21 +6,22 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
+app.app_context().push()
 
 class Todo(db.Model):
-    sno = db.Column(db.Integer, primary_key = True)
+    sno = db.Column(db.Integer, primary_key=True)
     Title = db.Column(db.String(200), nullable=False)
     desc = db.Column(db.String(200), nullable=False)
     date_cr = db.Column(db.DateTime, default=datetime.utcnow)
 
-    def __repr__(self) ->str:
+    def __repr__(self) -> str:
         return f"{self.sno} - {self.Title}"
+
 
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
-    
+
 if __name__ == '__main__':
-    app.run(debug=True , port = 8000)
+    app.run(debug=True, port=8000)
